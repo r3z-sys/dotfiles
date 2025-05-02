00c1fdd7e85dd9721b5bd0f1b4724af3abc4f698
Rezky Yuranda <rezky@atmaluhur.ac.id>
2025-05-02 13:33:27 +0700

    first commit for flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = 
+  { nixpkgs, self, ... } @inputs: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    lib = nixpkgs.lib;
+  in
+  {
+    nixosConfigurations = {
+      devzc0de = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+        specialArgs = {
+          inherit self inputs;
+          #de = "gnome";
+        };
+      };
+    };
+  };
+}
