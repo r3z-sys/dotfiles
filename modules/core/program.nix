49e5eb3af9c007f520d373cb11351f1f00aa698f
r3z-sys <rezky@atmaluhur.ac.id>
2025-05-14 16:10:18 +0700

    apalah

---
+++ modules/core/program.nix
@@ -0,0 +1,16 @@
+{
+  self,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+
+{
+  programs.firefox.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true; # recommended for most users
+    xwayland.enable = true; # Xwayland can be disabled.
+  };
+}
